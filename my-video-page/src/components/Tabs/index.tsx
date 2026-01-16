d325d645edefea432933a48aebf0a447781c0eda
林佳嫻 <record.life.start@gmail.com>
2026-01-16 17:52:41 +0800

    完成 吸頂的Tabs

--- my-video-page/src/components/Tabs/index.tsx
+++ my-video-page/src/components/Tabs/index.tsx
@@ -3,7 +3,13 @@ import styles from './styles.module.scss'
 
 const Tabs = () =>{
     return(
-        <div className={styles.tabs}>Tabs</div>
+        <div className={styles.tabs}>
+            <ul>
+                <li>大會員</li>
+                <li>消息</li>
+                <li>動態</li>
+            </ul>
+        </div>
     )
 }
 
